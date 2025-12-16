aa33059173d5cd2fda45be7a54556f1e19361a2a
Saisharath123 <Sai.sarath14@gmail.com>
2025-12-16 20:00:39 +0530

    16-12----8:pm

--- profile_page.py
+++ profile_page.py
@@ -296,11 +296,11 @@ def register_routes(app, render_page_func):
               </div>
 
               <div class="contact-box">
-                <a href="mailto:multiclouddevops4u@gmail.com" class="contact-link">
+                <a href="{{ url_for('contact') }}" class="contact-link">
                   <img src="{{ url_for('image_file', filename='gmail.png') }}" alt="Gmail" style="width:20px;height:20px;object-fit:contain;">
                   <span>multiclouddevops4u@gmail.com</span>
                 </a>
-                <a href="tel:+919666562012" class="contact-link">
+                <a href="{{ url_for('contact') }}" class="contact-link">
                   <svg viewBox="0 0 24 24"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
                   <span>+91 96665 62012</span>
                 </a>
